4e48252fd0feb7c4299fea1410de33c54091d897
lucianavarro25 <00109525@uca.edu.sv>
2026-09-17 20:35:24 -0600

    feat: agregar struct vehiculo

---
+++ ejercicio1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+struct vehiculo{
+    char placa[10];
+    char tipo[20];
+    int horaEntrada;
+    bool activo;
+};
+
+int main(){
+    return 0;
+}
+
